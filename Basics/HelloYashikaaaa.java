0650bd2d18b1e341b2412821b4b903615c5a541f
Vansh Pratap Singh Jadon <vanshpratapsinghjadon95@gmail.com>
2026-09-07 12:29:47 +0530

    Basics of java

---
+++ Basics/HelloYashikaaaa.java
@@ -0,0 +1,10 @@
+package Basic_Cheeze;
+
+public class HelloYashikaaaa {
+    public static void main(String[] args) {
+        System.out.print("Hello Kirti\n");
+        System.out.println("Hello Sumit");
+        System.out.println("Hello Rishika\n and Karan");
+
+    }
+}
